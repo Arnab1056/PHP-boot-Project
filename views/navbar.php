989dbbeac356951a8d8c7de888f950c4c4fafa9c
Arnab1056 <1056arnab@gmail.com>
2025-05-07 00:47:08 +0600

    all problems are fixed

--- views/navbar.php
+++ views/navbar.php
@@ -3,17 +3,23 @@
         <a class="navbar-brand" href="#">RBAC System</a>
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="dashboard.php">Dashboard</a>
-                </li>
-                <?php if (isset($_SESSION['role_id']) && $_SESSION['role_id'] == 1): ?>
+                <?php if (!isset($_SESSION['user_id'])): ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="admin_approval.php">Admin Approval</a>
+                        <a class="nav-link" href="../index.php">Login</a>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="dashboard.php">Dashboard</a>
+                    </li>
+                    <?php if ($_SESSION['role_id'] == 1): ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="admin_approval.php">Admin Approval</a>
+                        </li>
+                    <?php endif; ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
                     </li>
                 <?php endif; ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="logout.php">Logout</a>
-                </li>
             </ul>
         </div>
     </div>
